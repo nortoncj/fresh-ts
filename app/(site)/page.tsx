933eaa99f62ab3496e6ef522d4ff9ae6152923fd
Chris Norton <officialantiviral@gmail.com>
2023-07-18 23:38:48 -0400

    added photos

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-sky-500 text-3xl">
-      <p>Hello Home!</p>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-"></div>
     </div>
   );
 }
