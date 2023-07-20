67e88c8767366c145c13310d1a2a6f1e59e98f5b
Chris Norton <officialantiviral@gmail.com>
2023-07-19 20:38:25 -0400

    moved avatar

--- components/sidebar/DesktopSidebar.tsx
+++ components/sidebar/DesktopSidebar.tsx
@@ -16,8 +16,11 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
 
   console.log({ currentUser });
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
-      <nav className="mt-4 flex flex-col justify-between">
+    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col ">
+      <nav className=" mt-4 flex flex-col justify-between items-center">
+        <div className="cursor-pointer hover:opacity-75 transition" onClick={() => setIsOpen(true)}><Avatar user={currentUser} /></div>
+      </nav>
+      <nav className="mt-4 flex flex-col ">
         <ul role="list" className="flex flex-col items-center space-y-1">
           {routes.map((item) => (
             <DesktopItem
@@ -31,9 +34,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
           ))}
         </ul>
       </nav>
-      <nav className=" mt-4 flex flex-col justify-between items-center">
-        <div className="cursor-pointer hover:opacity-75 transition" onClick={() => setIsOpen(true)}><Avatar user={currentUser} /></div>
-      </nav>
+      
     </div>
   );
 };
