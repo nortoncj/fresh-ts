c4283ad35973779eb68fb704ed516623328c4325
Chris Norton <officialantiviral@gmail.com>
2023-07-21 23:39:47 -0400

    billoard categories

---
+++ app/api/[storeId]/categories/route.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const user = await getCurrentUser();
+    const userId = user?.id;
+
+    const body = await req.json();
+
+    const { name, billboardId } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!billboardId) {
+      return new NextResponse("Billboard ID is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 405 });
+    }
+
+    const category = await prismadb.category.create({
+      data: {
+        name,
+        billboardId,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORIES_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const categories = await prismadb.category.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(categories);
+  } catch (error) {
+    console.log("[CATEGORIES_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
