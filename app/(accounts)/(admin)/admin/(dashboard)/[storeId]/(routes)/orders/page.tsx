7054b88704408a2aa7acd9011dd0676d242bd5de
Chris Norton <officialantiviral@gmail.com>
2023-07-22 14:01:50 -0400

    orders done (untested)

--- app/(accounts)/(admin)/admin/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(accounts)/(admin)/admin/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { format } from "date-fns";
 
-const Orders = () => {
-  return <div>Orders</div>;
+import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+
+import { OrderColumn } from "./components/columns";
+import { OrderClient } from "./components/client";
+
+const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
+  const orders = await prismadb.order.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedOrders: OrderColumn[] = orders.map((item) => ({
+    id: item.id,
+    phone: item.phone,
+    address: item.address,
+    products: item.orderItems
+      .map((orderItem) => orderItem.product.name)
+      .join(", "),
+    image: item.orderItems
+    .map((orderItem) => orderItem.image)
+    .join(", "),
+    totalPrice: formatter.format(
+      item.orderItems.reduce((total, item) => {
+        return total + Number(item.product.price);
+      }, 0)
+    ),
+    isPaid: item.isPaid,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <OrderClient data={formattedOrders} />
+      </div>
+    </div>
+  );
 };
 
-export default Orders;
+export default OrdersPage;
