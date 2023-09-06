d6d962875d6da1ce3395512b70695a7de7da086c
Chris Norton <officialantiviral@gmail.com>
2023-09-05 21:01:10 -0400

    announcement bar fixes

--- app/(root)/(frontend)/components/announcmentBar.tsx
+++ app/(root)/(frontend)/components/announcmentBar.tsx
@@ -3,80 +3,63 @@ import React, { useState, useEffect } from "react";
 import { Fauna_One } from "next/font/google";
 const faunaOne = Fauna_One({ subsets: ["latin"], weight: "400" });
 
+
+
+ 
+
 export const AnnouncementBar = () => {
-  interface CountdownTimerProps {
-    targetDate: Date;
-  }
 
-  interface TimeRemaining {
-    days: number;
-    hours: number;
-    minutes: number;
-    seconds: number;
-  }
 
-  function CountdownTimer({ targetDate }: CountdownTimerProps) {
-    const calculateTimeRemaining = (): TimeRemaining => {
-      const now = new Date().getTime();
-      const difference = targetDate.getTime() - now;
-
-      if (difference <= 0) {
-        clearInterval(intervalId);
-        return {
-          days: 0,
-          hours: 0,
-          minutes: 0,
-          seconds: 0,
-        };
-      }
-
-      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-      return {
-        days,
-        hours,
-        minutes,
-        seconds,
-      };
-    };
-
-    const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>(
-      calculateTimeRemaining()
-    );
-    const intervalId = setInterval(() => {
-      setTimeRemaining(calculateTimeRemaining());
-    }, 1000);
-
-    useEffect(() => {
-      return () => clearInterval(intervalId);
-    }, [intervalId]);
-
-    return (
-      <div className="countdown-menu_numbers">
-        <div className="days" style={faunaOne.style}>
-          {timeRemaining.days}
-        </div>
-        <span> :</span>
-        <div className="hours" style={faunaOne.style}>
-          {timeRemaining.hours}
-        </div>
-        <span> :</span>
-        <div className="minutes" style={faunaOne.style}>
-          {timeRemaining.minutes}
-        </div>
-        <span> :</span>
-        <div className="seconds" style={faunaOne.style}>
-          {timeRemaining.seconds}
-        </div>
-      </div>
-    );
-  }
   const TargetDate = new Date("2024-01-01T00:00:00");
+
+  // Initialize state variables for the countdown
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  // Function to calculate and update the countdown
+  const updateCountdown = () => {
+    const currentDate = new Date();
+    const timeDifference = TargetDate.getTime() - currentDate.getTime();
+
+    if (timeDifference <= 0) {
+      // Target date has passed
+      //@ts-ignore
+      clearInterval(interval);
+      return;
+    }
+
+    const remainingDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const remainingHours = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const remainingMinutes = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const remainingSeconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    setDays(remainingDays);
+    setHours(remainingHours);
+    setMinutes(remainingMinutes);
+    setSeconds(remainingSeconds);
+  };
+
+  useEffect(() => {
+    // Calculate and update the countdown initially
+    updateCountdown();
+
+    // Update the countdown every second
+    const interval = setInterval(updateCountdown, 1000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
+
+
+  
+  
   return (
     <div className="announcement-bar top-nav__group">
       <div
@@ -86,11 +69,27 @@ export const AnnouncementBar = () => {
         Ship to: US
       </div>
       <div className="countdown-menu">
-        <div className="countdown-menu_messsage--desktop">
-          <h1 style={faunaOne.style}>GRAND OPENING</h1>
-        </div>
-        <CountdownTimer targetDate={TargetDate} />
-      </div>
+  <div className="countdown-menu_messsage--desktop">
+    <h1 style={faunaOne.style}>GRAND OPENING</h1>
+  </div>
+  <div className="countdown-menu_numbers">
+  <div className="days" style={faunaOne.style}>
+    {days}
+  </div>
+  <span> :</span>
+  <div className="hours" style={faunaOne.style}>
+    {hours}
+  </div>
+  <span> :</span>
+  <div className="minutes" style={faunaOne.style}>
+    {minutes}
+  </div>
+  <span> :</span>
+  <div className="seconds" style={faunaOne.style}>
+    {seconds}
+  </div>
+</div>
+</div> 
     </div>
   );
 };
