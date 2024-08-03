8d7623fb9c93e3d8cb48eb023c8c0bce2be20af8
Chris Norton <contact@chrisnortonjr.com>
2024-08-03 13:01:17 -0400

    new build new bar

--- app/(root)/(frontend)/shop/[productId]/page.tsx
+++ app/(root)/(frontend)/shop/[productId]/page.tsx
@@ -37,7 +37,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
 
       <div className="">
         <Container>
-          <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="px-4 py-10 sm:px-6 lg:px-8 ">
             <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
               <div className="">
                 <Gallery images={product.images} />
